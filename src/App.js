1807b2f8a1f22185738a51eb3047be7fb409995e
komal_b2u59 <komal_b2u59@tataneuskills.com>
2023-10-10 21:12:39 +0530

    Add To cart Function

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Header from './componenets/header/Header';
+import Footer from './componenets/footer/Footer';
+import {createBrowserRouter, createRoutesFromElements, Outlet, Route, RouterProvider}  from 'react-router-dom';
+import Home from './pages/Home';
+import { productsData } from './api/Api';
+import Signin from './pages/Signin';
+
+const Layout = () =>{
+  return(
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
+function App() {
+  const router = createBrowserRouter(createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} loader={productsData}></Route>
+        <Route path="/login" element={<Signin />}></Route>
+      </Route>
+      
+  ))
+  return (
+    <div className='font-bodyFont bg-gray-100'>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
